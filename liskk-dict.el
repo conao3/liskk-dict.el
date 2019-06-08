0545da9d28bf7b75622bb3c8221efee5836314b2
conao3 <conao3@gmail.com>
2019-06-09 00:57:50 +0900

    initial commit

---
+++ liskk-dict.el
@@ -0,0 +1,32 @@
+;;; liskk-dict.el --- Dictionaly for liskk.el       -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; Maintainer: Naoya Yamashita <conao3@gmail.com>
+;; Keywords: lisp settings
+;; Version: 0.1.0
+;; URL: https://github.com/conao3/liskk-dict.el
+;; Package-Requires: ((emacs "24.4"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+
+(provide 'liskk-dict)
+;;; liskk-dict.el ends here
+
